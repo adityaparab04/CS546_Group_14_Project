ee31e299469e91f4a90176cc878038336b12e682
adityaparab04 <adityaparab04@gmail.com>
2021-11-28 14:01:24 -0500

    index js for db updated

--- data/index.js
+++ data/index.js
@@ -1,5 +1,11 @@
+const reviewData = require('./reviews');
+const commentData = require('./comments');
 const characterData = require("./anime");
+const animeData = require('./animeDb');
 
-module.exports ={
-    character : characterData
-}
+module.exports = {
+    reviews: reviewData,
+    comments: commentData,
+    character : characterData,
+    anime: animeData
+};
