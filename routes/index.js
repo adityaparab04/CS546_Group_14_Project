4944672baf813e5438685bd02dad9be16b09cfbe
adityaparab04 <adityaparab04@gmail.com>
2021-12-09 16:58:12 -0500

    modifications

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,25 @@ const characterRoute = require("./anime");
 const searchRoute = require("./search");
 const homeRoute = require("./home");
 const userRoute = require("./users");
+const reviewRoute = require("./reviews");
+const commentRoutes = require("./comments");
+const adminRoutes = require('./admin');
 
 const constructerMethod = (app) => {
   app.use("/", homeRoute);
   app.use("/search", searchRoute);
   app.use("/anime", characterRoute);
   app.use("/users", userRoute);
+  app.use("/reviews", reviewRoute);
+  app.use("/comments", commentRoutes);
+  app.use("/admin", adminRoutes);
 
   app.use("*", (req, res) => {
     //res.redirect("/");  
     const animeData = {
       error: "Route not found",
-      isUserLoggedIn:req.session.user!=null?true:false
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn: req.session.user != null ? true : false
     };
     res.status(404);
     res.render("anime/error", animeData);
