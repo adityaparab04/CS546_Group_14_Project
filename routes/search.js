82eaf98685c49db05ebf33e8fa537e1002e782c2
adityaparab04 <adityaparab04@gmail.com>
2021-12-09 17:00:19 -0500

    search js changes

--- routes/search.js
+++ routes/search.js
@@ -14,6 +14,8 @@ router.post("/", async (req, res) => {
     //errors.push("please provide a character");
     const animeData = {
       error: "please provide a character",
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn: req.session.user != null ? true : false
     };
     res.status(400);
     res.render("anime/error", animeData);
@@ -23,6 +25,8 @@ router.post("/", async (req, res) => {
     //errors.push("character is empty");
     const animeData = {
       error: "character is empty",
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn: req.session.user != null ? true : false
     };
     res.status(400);
     res.render("anime/error", animeData);
@@ -33,9 +37,11 @@ router.post("/", async (req, res) => {
     //errors.push("Character is not of type string")
     const animeData = {
       error: "Character is not of type string",
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn: req.session.user != null ? true : false
     };
     res.status(400);
-    res.render("anime/error",animeData);
+    res.render("anime/error", animeData);
     return;
   }
 
@@ -48,12 +54,16 @@ router.post("/", async (req, res) => {
       title: "Characters Found",
       newCharacter: newCharacter,
       character: animeCharacter,
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn: req.session.user != null ? true : false
     };
     res.render("anime/search", animeData);
   } catch (e) {
 
     const animeData = {
       error: e,
+      isAdmin: req.session.user && req.session.user.username.includes("admin") ? true : false,
+      isUserLoggedIn:req.session.user!=null?true:false
     };
     res.status(500);
     res.render("anime/error", animeData);
