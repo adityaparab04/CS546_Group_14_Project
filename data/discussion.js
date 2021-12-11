369aaf2150b22f1d478611f5d4cb2af0c20d389d
adityaparab04 <adityaparab04@gmail.com>
2021-12-11 00:04:00 -0500

    error handling for discussion anime and comments

--- data/discussion.js
+++ data/discussion.js
@@ -4,6 +4,11 @@ const discussion = mongoCollections.discussion;
 
 let exportedMethods = {
     async addDiscussion(animeId, epno, userId, text) {
+        if(!animeId || typeof animeId !== 'string') throw `invalid anime id`;
+        if(!epno || typeof epno !== 'string') throw `invalid ep no`;
+        if(!userId || typeof userId !== 'string') throw `invalid user id`;
+        if(!text || typeof text !== 'string') throw `invalid type of text`;
+        if(!text.replace(/\s/g, "").length) throw `Input cannot be empty spaces`;
         let discussionCollection = await discussion();
         let insertInfo = await discussionCollection.insertOne({
             animeId,
@@ -18,6 +23,8 @@ let exportedMethods = {
         return `discussion inserted`;
     },
     async getAllDiscussionsOfAnAnimeEpisode(animeId, epno) {
+        if(!animeId || typeof animeId !== 'string') throw `invalid anime id`;
+        if(!epno || typeof epno !== 'string') throw `invalid ep no`;
         let discussionCollection = await discussion();
         let allDis = await discussionCollection.find({ animeId, epno }).toArray();
         return allDis;
