689192b2901260332c9103f36d63c8d3a391391b
adityaparab04 <adityaparab04@gmail.com>
2021-12-11 12:07:23 -0500

    error handling for comments and seed change

--- tasks/seed.js
+++ tasks/seed.js
@@ -6,17 +6,28 @@ const comments = data.comments;
 const animeData = require("../data/anime");
 
 async function main() {
-  // const db = await connection.connectToDb();
+  const db = await connection.connectToDb();
   // await db.dropDatabase();
 
-  // await connection.closeConnection();
+  //users.deleteUser('61b43fc1a49a119341eafe80')
 
-  try{
-    const anime = await animeData.getOngoingAnime();
-    console.log(anime);
-  }catch(e){
-    console.log(e);
-  }
+
+  // try{
+  //   const user1 = await users.createUser('Vaibhav1', 25, '123456', 'vaibhav1@123.com', '/public/images/profile_pic.jpeg');
+  // }catch(e){
+  //   console.log(e);
+  // }
+
+  // try{
+  //   const user1 = await users.deleteUser('61b4d5be6d6915a5d488a3a4')
+  //   console.log(user1);
+  // }catch(e){
+  //   console.log(e);
+  // }
+
+  await reviews.removeReview('61b4da49f454090324d6fada')
+
+  await connection.closeConnection();
 }
 
 main();
